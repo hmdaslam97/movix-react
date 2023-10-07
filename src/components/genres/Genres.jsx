f4c4e4d2986d7e2047f0b6514e77a166ecc7d171
Mohammed Aslam <hmdaslam97@gmail.com>
2023-10-07 17:22:19 +0530

    first commit

---
+++ src/components/genres/Genres.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.scss'
+import {useSelector} from "react-redux"
+
+const Genres = ({data}) => {
+    const {genres} = useSelector((state)=>state.home);
+
+  return <div className="genres">
+    {data?.map((g, index)=>{
+        if(!genres[g]?.name) return;
+        return <div className="genre" key={index+1}>
+            {genres[g]?.name}
+        </div>
+    })}
+  </div>
+}
+
+export default Genres
